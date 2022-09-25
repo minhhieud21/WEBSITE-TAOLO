dd3330994e4bba5f64c391dfced6ec283afdd5e9
HectorLam <hienlam748@gmail.com>
2022-09-25 15:01:30 +0000

    Tao HD thêm api nè

--- back-end/src/main/java/com/example/JavaSpring/controllers/BlogController.java
+++ back-end/src/main/java/com/example/JavaSpring/controllers/BlogController.java
@@ -36,7 +36,7 @@ public class BlogController {
             Date date = new Date();
             String content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
                     "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer " +
-                    "took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, "
+                    "took a gasslley of type and scrambled it to make a type specimen book. It has survived not only five centuries, "
                     ;
             for(int i = 1; i<=21;i++){
                 blogService.saveBlog(new BlogModel(Long.valueOf(i),"Blog "+i, content+i,i,i,dateFormat.format(date)));
